c116262c22fa254ef708a65640f9f42f59ae8330
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-11 23:34:26 +0000

    add fallback mongodb url for local hosting

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   DB: {
     test: 'mongodb://localhost/northcoders-news-api-test',
-    dev: process.env.MONGO_URI
+    dev: process.env.MONGO_URI || 'mongodb://localhost/northcoders-news-api-dev'
   },
   PORT: {
     test: 3090,
