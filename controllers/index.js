f6b5c040b3b75ab5fd1ed8c7c558e33c7baa6d4c
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-14 20:32:51 +0000

    sort comments by time

--- controllers/index.js
+++ controllers/index.js
@@ -11,7 +11,7 @@ module.exports = {
     const {article_id: belongs_to} = req.params;
     Comments.find({belongs_to})
       .then(comments => {
-        res.status(200).send({comments});
+        res.status(200).send({comments: comments.sort((a,b) => b.created_at - a.created_at)});
       })
       .catch(err => {
         if(err.name === 'CastError')   next({status: 404, message: 'ARTICLE_ID NOT FOUND'});
